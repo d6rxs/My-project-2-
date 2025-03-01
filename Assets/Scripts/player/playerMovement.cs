cce8d74ee9df4c542055720129c290111046548f
Rafael Quesada Ferrer <rafaelquesadaferrer@gmail.com>
2025-03-01 21:00:44 +0100

    sillas modulares

--- Assets/Scripts/player/playerMovement.cs
+++ Assets/Scripts/player/playerMovement.cs
@@ -10,13 +10,12 @@ public class PlayerMovement : MonoBehaviour
     // public vars
     public Camera playerCamera;
     public Animator animator;
-    public GameObject sitPosition1;
-    public GameObject standPosition;
     public GameObject circle;
     public bool isSitting = false;
     public float gravity = 10f;
     public float lookXLimit = 100f;
     public float lookYLimit = 0f;
+    public GameObject chair;
     
     [SerializeField] public float walkSpeed = 0.9f;
     [SerializeField] public float lookSpeed = 2f;
@@ -54,8 +53,6 @@ public class PlayerMovement : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         characterController.height = defaultHeight;
         Cursor.visible = false;
-        sitPosition1 = GameObject.Find("SitPosition1");
-        standPosition = GameObject.Find("StandPosition");
         circle.SetActive(false);
     }
 
@@ -160,18 +157,16 @@ public class PlayerMovement : MonoBehaviour
             RaycastHit hit;
             Ray ray = playerCamera.ScreenPointToRay(mousePos);
 
-            if (Physics.Raycast(ray, out hit, 1))
+            if (Physics.Raycast(ray, out hit, 1) && hit.collider.tag == "ChairG")
             {
-                if (hit.collider.tag == "ChairG")
-                {
-                    canMove = false;
-                    isSitting = true;
-                    lookXLimit = 30f;
-                    lookYLimit = 30f;
-                    animator.SetBool("SittingAnimation", isSitting);
-
-                    SitPosition();
-                }
+                chair = hit.collider.gameObject;
+                canMove = false;
+                isSitting = true;
+                lookXLimit = 30f;
+                lookYLimit = 30f;
+                animator.SetBool("SittingAnimation", isSitting);
+                SitPosition();
+                Debug.Log(hit.collider.gameObject.name);
             }
         }
 
@@ -179,9 +174,7 @@ public class PlayerMovement : MonoBehaviour
         {
             animator.SetBool("SittingAnimation", false);
             await Task.Delay(1000);
-
             StandPosition();
-
             isSitting = false;
             canMove = true;
             lookXLimit = 100f;
@@ -231,8 +224,9 @@ public class PlayerMovement : MonoBehaviour
         characterController.enabled = false;
 
         // Mueve al jugador a la posición y rotación del GameObject SitPosition1
-        transform.position = sitPosition1.transform.position;
-        transform.rotation = sitPosition1.transform.rotation;
+        transform.position = chair.transform.GetChild(0).gameObject.transform.position;
+        transform.rotation = chair.transform.GetChild(0).gameObject.transform.rotation; 
+
     }
 
     void StandPosition()
@@ -241,8 +235,8 @@ public class PlayerMovement : MonoBehaviour
         characterController.enabled = true;
 
         // Mueve al jugador a la posición y rotación del GameObject SitToStand
-        transform.position = standPosition.transform.position;
-        transform.rotation = standPosition.transform.rotation;
+        transform.position = chair.transform.GetChild(1).transform.gameObject.transform.position;
+        transform.rotation = chair.transform.GetChild(1).transform.gameObject.transform.rotation;
     }
 
     void AdjustFOV(float speedX, float speedY)
